78d0a4a066ab1abf56ff99c80b4e99508c253f59
prjktt <gop2p.io@gmail.com>
2026-06-30 20:49:59 +0400

    helmoragent 0.1.2

--- Formula/helmoragent.rb
+++ Formula/helmoragent.rb
@@ -1,8 +1,8 @@
 class Helmoragent < Formula
   desc "Agent watcher for AI-assisted product development"
   homepage "https://github.com/helmorx/agent-os"
-  url "https://github.com/helmorx/agent-os/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "7df75614949a7246e610ba089845737a8c100bd120fe57ef1fe922a5406fa4de"
+  url "https://github.com/helmorx/agent-os/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "6695fec0a230d70442fe488cd9b73605b227169f50443c57d90b12cce864a314"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -12,7 +12,7 @@ class Helmoragent < Formula
   end
 
   test do
-    assert_match "0.1.1", shell_output("#{bin}/helmor version")
+    assert_match "0.1.2", shell_output("#{bin}/helmor version")
     assert_match "HELMOR Agent OS", shell_output("#{bin}/helmor help")
   end
 end
